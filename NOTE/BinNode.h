6e26b71a1a41b64fd34095854d9fbaa0d0409bd9
mjcharlie993 <jiachen_mao@163.com>
2017-03-21 17:18:50 +0800

    Update BinNode.h and BinTree.cpp

--- NOTE/BinNode.h
+++ NOTE/BinNode.h
@@ -1,32 +1,33 @@
 // 二叉树节点模板类
 #define BinNodePosi(T) BinNode<T>*
 #define stature(p) ((p)? p->height : -1)
-typedef enum {RB_RED, RB_BLACK} RBColor;
+typedef enum { RB_RED, RB_BLACK } RBColor;
 
 template<typename T> struct BinNode {
-    T data;
-    BinNodePosi(T) parent; BinNodePosi(T) lc; BinNodePosi(T) rc;
-    int height;
-    int npl;
-    RBColor color;
+	T data;
+	BinNodePosi(T) parent; BinNodePosi(T) lc; BinNodePosi(T) rc;
+	int height;
+	int npl;
+	RBColor color;
 
-    BinNode() : 
-        parent(nullptr), lc(nullptr), rc(nullptr), height(0), npl(1), color(RB_RED) {}
-    BinNode(T e, BinNodePosi(T) p = nullptr, BinNodePosi(T) lc = nullptr, BinNodePosi(T) rc = nullptr, int h = 0, int npl = 1, RBColor = RB_RED) :
-        data(e), parent(p), lc(lc), rc(rc), height(h), npl(l), color(c) {}
-    
-    int size();
-    BinNodePosi(T) insertAsLC(T const&);
-    BinNodePosi(T) insertASRC(T const&);
-    BinNodePosi(T) succ();
-    template<typename VST> void travLevel(VST&);
-    template<typename VST> void travPre(VST&);
-    template<typename VST> void travIn(VST&);
-    template<typename VST> void travPost(VST&);
+	BinNode() :
+		parent(nullptr), lc(nullptr), rc(nullptr), height(0), npl(1), color(RB_RED) {}
+	BinNode(T e, BinNodePosi(T) p = nullptr, BinNodePosi(T) lc = nullptr, BinNodePosi(T) rc = nullptr, int h = 0, int npl = 1, RBColor = RB_RED) :
+		data(e), parent(p), lc(lc), rc(rc), height(h), npl(l), color(c) {}
 
-    bool operator< (BinNode const& bn) { return data < bn.data; }
-    bool operator== (BinNode const& bn) { return data == bn.data; }
+	int size();
+	BinNodePosi(T) insertAsLC(T const&);
+	BinNodePosi(T) insertASRC(T const&);
+	BinNodePosi(T) succ();
+	template<typename VST> void travLevel(VST&);
+	template<typename VST> void travPre(VST&);
+	template<typename VST> void travIn(VST&);
+	template<typename VST> void travPost(VST&);
+
+	bool operator< (BinNode const& bn) { return data < bn.data; }
+	bool operator== (BinNode const& bn) { return data == bn.data; }
 };
+
 
 // BinNode状态与性质的判断
 #define IsRoot(x) (!((x).parent))
@@ -40,16 +41,17 @@ template<typename T> struct BinNode {
 #define IsLeaf(x) (!HasChild(x))
 
 // 与BinNode具有特定关系的节点及指针
-#define sibling(p) /*兄弟*/ 
-    (IsLChild(*(p)) ? (p)->parent->rc : (p)->parent->lc)
-#define uncle(x) 
-    (IsLChild(*((x)->parent)) ? (x)->parent->parent->rc : (x)->parent->parent->lc)
-#define FromParentTo(x)
-    (IsRoot(x) ? _root : (IsLChild(x) ? (x).parent->lc : (x).parent->rc))
+#define sibling(p) /*兄弟*/ (IsLChild(*(p)) ? (p)->parent->rc : (p)->parent->lc)
+#define uncle(x) (IsLChild(*((x)->parent)) ? (x)->parent->parent->rc : (x)->parent->parent->lc)
+#define FromParentTo(x) (IsRoot(x) ? _root : (IsLChild(x) ? (x).parent->lc : (x).parent->rc))
 
 // 插入孩子节点
-template<typename T> BinNodePosi(T) BinNode<T>::insertAsLC(T const& e) 
-{ return lc = new BinNode(e, this); }
-template<typename T> BinNodePosi(T) BinNode<T>::insertAsRC(T const& e) 
-{ return rc = new BinNode(e, this); }
+template<typename T> BinNodePosi(T) BinNode<T>::insertAsLC(T const& e)
+{
+	return lc = new BinNode(e, this);
+}
+template<typename T> BinNodePosi(T) BinNode<T>::insertAsRC(T const& e)
+{
+	return rc = new BinNode(e, this);
+}
 
--- NOTE/BinTree.cpp
+++ NOTE/BinTree.cpp
@@ -30,3 +30,93 @@ public:
 	bool operator== (BinTree<T> const& t) { return _root && t._root && (_root == t._root); }
 }; 
 
+template<typename T> int BinTree<T>::updateHeight(BinNodePosi(T) x)
+{
+	return x->height = 1 + max(stature(x->lc), stature(x->rc));
+}
+template<typename T> void BinTree<T>::updateHeightAbove(BinNodePosi(T) x)
+{
+	while (x) {
+		updateHeight(x);
+		x = x->parent;
+	}
+}
+template<typename T> BinNodePosi(T) BinTree<T>::insertAsRoot(T const& e)
+{
+	_size = 1;
+	return _root = new BinNode<T>(e);
+}
+template<typename T> BinNodePosi(T) BinTree<T>::insertAsLC(BinNodePosi(T) x, T const& e)
+{
+	_size++;
+	x->insertAsLC(e);
+	updateHeightAbove(x);
+	return x->lc;
+}
+template<typename T> BinNodePosi(T) BinTree<T>::insertAsRC(BinNodePosi(T) x, T const& e)
+{
+	_size++;
+	x->insertASRC(e);
+	updateHeightAbove(x);
+	return x->rc;
+}
+template<typename T> 
+BinNodePosi(T) BinTree<T>::attachAsLC(BinNodePosi(T) x, BinTree<T>* &S)
+{
+	if (x->lc = S->_root)
+		x->lc->parent = x;
+	_size += S->size;
+	updateHeightAbove(x);
+	S->_root = nullptr;
+	S->_size = 0;
+	release(S);
+	S = nullptr;
+	return x;
+}
+template<typename T> 
+BinNodePosi(T) BinTree<T>::attachAsRC(BinNodePosi(T) x, BinTree<T>* &S)
+{
+	if (x->rc = S->_root)
+		x->rc->parent = x;
+	_size += S->size;
+	updateHeightAbove(x);
+	S->_root = nullptr;
+	S->_size = 0;
+	release(S);
+	S = nullptr;
+	return x;
+}
+
+template<typename T>
+int BinTree<T>::remove(BinNodePosi(T) x)
+{
+	FromParentTo(*x) = nullptr;
+	updateHeightAbove(x->parent);
+	int n = removeAt(x);
+	_size -= n;
+	return n;
+}
+template<typename T>
+static int removeAt(BinNodePosi(T) x)
+{
+	if (!x)
+		return 0;
+	int n = 1 + removeAt(x->lc) + removeAt(x->rc);
+	release(x->data);
+	release(x);
+	return n;
+}
+template<typename T> 
+BinTree<T>* BinTree<T>::secede(BinNodePosi(T) x)
+{
+	FromParentTo(*x) = nullptr;
+	updateHeightAbove(x->parent);
+	BinTree<T>* S = new BinTree<T>;
+	S->_root = x;
+	x->parent = nullptr;
+	S->_size = x->size();
+	_size -= S->_size;
+	return S;
+}
+
+// 就二叉树拓扑结构的变化范围而言，以上算法均只涉及局部的常数个节点。因此，除了更新祖先高度和释放节点等操作，只需常数时间

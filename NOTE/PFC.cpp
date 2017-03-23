98be923a0456d4e0e9cfe7f50b77281c22a7e618
mjcharlie993 <jiachen_mao@163.com>
2017-03-23 09:56:10 +0800

    update PFC.cpp

--- NOTE/PFC.cpp
+++ NOTE/PFC.cpp
@@ -41,6 +41,15 @@ PFCForest* initForest() {   // PFC编码森林初始化
 PFCTree* generateTree(PFCForest* forest) {
     srand ((unsigned int)time(NULL));
     while (1 < forest->size()) {
-        
+        PFCTree* s = new PFCTree;
+        s->insertAsRoot('^');
+        Rank r1 = rand() % forest->size();
+        s->attachAsLC(s->root(), (*forest)[r1]);
+        forest->remove(r1);
+        Rank r2 = rand() % forest->size();
+        s->attachAsRC(s->root(), (*forest)[r2]);
+        forest->remove(r2);
+        forest->insert(forest->size(), s);
     }
+    return (*forest)[0];
 }

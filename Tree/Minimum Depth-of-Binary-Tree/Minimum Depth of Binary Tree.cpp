e61a016d9444633642deb1a3140dc26a92089755
mjcharlie993 <jiachen_mao@163.com>
2017-03-27 22:33:33 +0800

    Minimum Depth of Binary Tree

---
+++ Tree/Minimum Depth-of-Binary-Tree/Minimum Depth of Binary Tree.cpp
@@ -0,0 +1,26 @@
+//Source : https://leetcode.com/problems/minimum-depth-of-binary-tree/
+//Author : Mao Jiachen
+//Date   : 2017/03/27
+
+/********************************************************************
+ * 
+ * 
+ *******************************************************************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(!root) return 0;
+        if(!root->left) return 1 + minDepth(root->right);
+        if(!root->right) return 1 + minDepth(root->left);
+        return 1+min(minDepth(root->left),minDepth(root->right));
+    }
+};

8488c24d55dc8b0dd94600ad3a5d3b776454c38d
mjcharlie993 <jiachen_mao@163.com>
2017-04-01 16:17:58 +0800

    Same Tree.cpp

---
+++ Tree/Same-Tree/Same Tree.cpp
@@ -0,0 +1,17 @@
+//Source : https://leetcode.com/problems/same-tree/
+//Author : Mao Jiachen
+//Date   : 2017/04/01
+
+/********************************************************************
+ * 
+ * 
+ *******************************************************************/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */

9f27d82a1104280bbf56ec15cd20ccf1c50a7fba
mjcharlie993 <jiachen_mao@163.com>
2017-06-07 17:19:41 +0800

    Create 3Sum

---
+++ Array/3Sum/3Sum.cpp
@@ -0,0 +1,64 @@
+//Source : https://leetcode.com/problems/3sum/
+//Author : Mao Jiachen
+//Date   : 2017/06/06
+
+/****************************************************************************************
+ *
+ * Approach1: Two Pointer
+ * 
+ * Approach2: Use set
+ *
+ ***************************************************************************************/
+
+// #1
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for (int k = 0; k != nums.size(); k++) {
+            if (nums[k] > 0) break;
+            if (k > 0 && nums[k] == nums[k - 1]) continue;
+            int target = 0 - nums[k];
+            int i = k + 1, j = nums.size() - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] == target) {
+                    res.push_back({nums[k], nums[i], nums[j]});
+                    while (i < j && nums[i] == nums[i + 1]) i++;
+                    while (i < j && nums[j] == nums[j - 1]) j--;
+                    i++;
+                    j--;
+                }
+                else if (nums[i] + nums[j] < target)
+                    i++;
+                else
+                    j--;
+            }
+        }
+        return res;
+    }
+};
+
+// #2
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for (int k = 0; k < nums.size(); ++k) {
+            if (nums[k] > 0) break;
+            int target = 0 - nums[k];
+            int i = k + 1, j = nums.size() - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] == target) {
+                    res.insert({nums[k], nums[i], nums[j]});
+                    while (i < j && nums[i] == nums[i + 1]) ++i;
+                    while (i < j && nums[j] == nums[j - 1]) --j;
+                    ++i; --j;
+                } else if (nums[i] + nums[j] < target) ++i;
+                else --j;
+            }
+        }
+        return vector<vector<int>>(res.begin(), res.end());
+    }
+};
